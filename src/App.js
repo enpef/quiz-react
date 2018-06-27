5eeb727529792e726fe2a702d001ab4e88c14f44
unknown <natdanai.prs@gmail.com>
2018-06-27 22:17:50 +0700

    create total function to calculate result

--- src/App.js
+++ src/App.js
@@ -4,26 +4,37 @@ import Input from './input';
 class App extends Component {
 
   state = {
-    row : [],
-    row_count : 0
+    row : [0],
+    total : 0
   }
 
   Add = () => {
     const row = this.state.row;
       this.setState({
-          row: row.concat(this.state.row_count+1),
-          row_count: this.state.row_count+1
+          row: row.concat(this.state.row.length)
       });
+  }
+
+  Total = () => {
+    const count = this.state.row
+    var total = 0
+    for(const i in count){
+      if(document.getElementById(i) != null){
+        var num = Number(document.getElementById(i).innerHTML)
+        total += num
+      }
+    }
+    document.getElementById("total").innerHTML = "Total : " + total
+    console.log(total)
   }
 
   render() {
     const itemlist = this.state.row.map( (row) => {
-      return <Input key={row} />
+      return <Input total={this.Total} id={row} key={row} />
     })
     return (
       <div className="App">
-        <button type="button" onClick={this.Add} >Add</button>
-        <Input />
+        <button type="button" onClick={this.Add} >Add</button> <span id="total">Total : </span>
         {itemlist}
       </div>
     );
